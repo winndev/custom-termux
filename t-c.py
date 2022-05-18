f6adf1da58995aa8e2e5258644bb5c49da2e83e9
winndev <56520730+winndev@users.noreply.github.com>
2022-05-18 14:11:16 -0300

    Update t-c.py

--- t-c.py
+++ t-c.py
@@ -1,10 +1,32 @@
 import os,sys,time,random
+def menu():
 print("")
 print("")
 color = ["\033[1;31m","\033[1;32m"]
-m = random.choice(color)+"Bem vindo THBD! \n Mais informações em: https://github.com/winndev \n"
+m = random.choice(color)+"Bem vindo THBD! \n Para mais informações, insira '1' \n\n"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
     time.sleep(0.06)
 print("")
+else:
+break
+
+loop = True
+
+while loop:
+    menu()
+    what = input("#: ")
+if what == "1":
+        print("====================================")
+        print("[+] Github: https://github.com/winndev")
+        print("[+] Twitter: https://twitter.com/winnhacking")
+        print("[+] Instagram: https://instagram.com/winntralha")
+        print("[+] Discord: winndev#0606")
+        print("[+] Site: https://shadowmans.tk")
+        print("====================================")
+        rmenu = input("[?] Voltar para o menu principal ? (s/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
