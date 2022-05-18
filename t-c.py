1eac627e591f789e331e679841f43571df574546
winndev <56520730+winndev@users.noreply.github.com>
2022-05-18 13:14:54 -0300

    Update t-c.py

--- t-c.py
+++ t-c.py
@@ -2,7 +2,7 @@ import os,sys,time,random
 print("")
 print("")
 color = ["\033[1;31m","\033[1;32m"]
-m = random.choice(color)+"Bem vindo THBD! \n Mais informações em: https://github.com/winndev\n"
+m = random.choice(color)+"Bem vindo THBD! \n Mais informações em: https://github.com/winndev \n"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
