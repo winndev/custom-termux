e2f7700eaae6697a2d0ea716bc54d8c17942077b
winndev <56520730+winndev@users.noreply.github.com>
2022-05-18 14:33:33 -0300

    Update t-c.py

--- t-c.py
+++ t-c.py
@@ -2,7 +2,7 @@ import os,sys,time,random
 print("")
 print("")
 color = ["\033[1;31m","\033[1;32m"]
-m = random.choice(color)+"Bem vindo THBD! \n Para mais informações, insira '1' \n\n"
+m = random.choice(color)+"Bem vindo THBD! \n Para mais informações, acesse "https://shadowmans.tk" \n\n"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
